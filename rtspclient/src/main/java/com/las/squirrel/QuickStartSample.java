05f011dc3f979126ffabbd1de14bf2378c1407a8
las <las@sengled.com>
2018-10-12 19:00:27 +0800

    squirrel test

--- rtspclient/src/main/java/com/las/squirrel/QuickStartSample.java
+++ rtspclient/src/main/java/com/las/squirrel/QuickStartSample.java
@@ -17,15 +17,19 @@ public class QuickStartSample {
         ToA, ToB, ToC, ToD
     }
 
+    enum State {
+        A, B, C, D
+    }
+
     // 2. Define State Machine Class
-    @StateMachineParameters(stateType = String.class, eventType = FSMEvent.class, contextType = Integer.class)
+    @StateMachineParameters(stateType = State.class, eventType = FSMEvent.class, contextType = Integer.class)
     static class StateMachineSample extends AbstractUntypedStateMachine {
-        protected void fromAToB(String from, String to, FSMEvent event, Integer context) {
+        protected void fromAToB(State from, State to, FSMEvent event, Integer context) {
             System.out.println("Transition from '" + from + "' to '" + to + "' on event '" + event +
                     "' with context '" + context + "'.");
         }
 
-        protected void ontoB(String from, String to, FSMEvent event, Integer context) {
+        protected void ontoB(State from, State to, FSMEvent event, Integer context) {
             System.out.println("Entry State \'" + to + "\'.");
         }
     }
@@ -34,14 +38,14 @@ public class QuickStartSample {
     public static void main(String[] args) {
         // 3. Build State Transitions
         UntypedStateMachineBuilder builder = StateMachineBuilderFactory.create(StateMachineSample.class);
-        builder.externalTransition().from("A").to("B").on(FSMEvent.ToB).callMethod("fromAToB");
-        builder.onEntry("B").callMethod("ontoB");
+        builder.externalTransition().from(State.A).to(State.B).on(FSMEvent.ToB).callMethod("fromAToB");
+        builder.onEntry(State.B).callMethod("ontoB");
 
         // 4. Use State Machine
-        UntypedStateMachine fsm = builder.newStateMachine("A");
+        UntypedStateMachine fsm = builder.newStateMachine(State.A);
         fsm.fire(FSMEvent.ToB, 10);
 
-        System.out.println("Current state is "+fsm.getCurrentState());
+        System.out.println("Current state is " + fsm.getCurrentState());
     }
 
 }
